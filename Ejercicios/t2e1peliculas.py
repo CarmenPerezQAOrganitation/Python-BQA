7ff20a269f9185dbf633f90d2e89c946bfd479d3
Carmen Pérez <cperezrobles@gmail.com>
2026-06-02 21:00:55 +0200

    Update t2e1peliculas.py

---
+++ Ejercicios/t2e1peliculas.py
@@ -0,0 +1,25 @@
+# Cadenas (strings)
+Titulo = "Los amantes del círculo polar"
+
+# Cadenas
+Director = "Julio Medem"
+
+# Números enteros (int)
+Año_lanzamiento = 1998
+
+# Cadenas
+Genero = "Romance, Drama"
+
+# Números decimales (float)
+Duración = 120.5
+
+# Booleanos
+Premiada = True
+
+# Imprimir la información de la película
+print("Título:", Titulo)
+print("Director:", Director)
+print("Año de lanzamiento:", Año_lanzamiento)
+print("Género:", Genero)
+print("Duración (minutos):", Duración)
+print("¿La película ha sido premiada?", Premiada)
