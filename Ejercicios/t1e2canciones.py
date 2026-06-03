a993e228b100c539a1c40165c616f716f275686a
Carmen Pérez <cperezrobles@gmail.com>
2026-06-03 12:43:18 +0200

    Update t1e2canciones.py

--- Ejercicios/t1e2canciones.py
+++ Ejercicios/t1e2canciones.py
@@ -1,23 +1,18 @@
-# Obtener un valor de la usuaria
+# Cancion favorita
 canción = input("¿Cuál es tu canción favorita? ")
 
-# Obtener un valor de la usuaria
 artista = input("¿Quién es el artista?")
 
-# Obtener un valor de la usuaria
 álbum = input("¿A qué álbum pertenece la canción?")
 
-# Convertir la entrada a un número entero
 año_de_lanzamiento = int(input("¿En qué año se lanzo? "))
 
-# Convertir la entrada a un número decimal
 segundos = float(input("¿Cuánto dura la canción? "))
 
-# Obtener un valor de la usuaria
 videoclip = input("¿Tiene videoclip? ") == "sí"
 
 
-#imprimir toda la información de la canción
+#mostrar la información de la canción
 print("Canción:", canción)
 print("Artista:", artista)
 print("Álbum:", álbum)
