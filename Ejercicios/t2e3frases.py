cc90143f8a14df9dfaeeeaa4ae8cfb646741208c
Carmen Pérez <cperezrobles@gmail.com>
2026-06-03 18:49:31 +0200

    Ejercicios

---
+++ Ejercicios/t2e3frases.py
@@ -0,0 +1,15 @@
+# Pide a la usuaria una frase
+frase = input("Ingresa una frase: ")
+
+#Longitud de la frase
+texto = frase.replace(" ", "")  # Elimina los espacios para contar solo las letras
+longitud = len(texto)
+print(longitud)  # Salida: 10
+
+# Convertir a mayúsculas
+mayusculas = frase.upper()
+print(mayusculas) 
+
+# Convertir a minusculas
+minusculas = frase.lower()
+print(minusculas)  
