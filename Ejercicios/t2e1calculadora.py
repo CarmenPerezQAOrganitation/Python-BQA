e8f0dcab74ae04c113b4639474694fb11b31381f
Carmen Pérez <cperezrobles@gmail.com>
2026-06-03 13:10:25 +0200

    Update t2e1calculadora.py

---
+++ Ejercicios/t2e1calculadora.py
@@ -0,0 +1,16 @@
+#Pedir al usuario que introduzca dos números.
+#Convertimos a numero
+numero1= float(input("Introduce un numero: "))
+numero2= float(input("Introduce el segundo número: "))
+
+#Hacemos operaciones
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+#mostrar resultados
+print("suma", suma)
+print("resta", resta)
+print("multiplicacion", multiplicacion)
+print("division", division)
