c4167e096e4a37fc8fac45f7342e235960eabf9e
Carmen Pérez <cperezrobles@gmail.com>
2026-06-03 11:51:48 +0200

    Update t2e1peliculas.py

--- Ejercicios/t2e1peliculas.py
+++ Ejercicios/t2e1peliculas.py
@@ -23,3 +23,30 @@ print("Año de lanzamiento:", Año_lanzamiento)
 print("Género:", Genero)
 print("Duración (minutos):", Duración)
 print("¿La película ha sido premiada?", Premiada)
+
+#Cambiar datos a inglés
+# Cadenas (strings)
+Title = "Lovers of the Polar Circle"
+
+# Cadenas
+Director = "Julio Medem"
+
+# Números enteros (int)
+Release_year = 1998
+
+# Cadenas
+Genre = "Romance, Drama"
+
+# Números decimales (float)
+Duration = 120.5
+
+# Booleanos
+Awarded = True
+
+# Imprimir la información de la película en ingles
+print("Title:", Title)
+print("Director:", Director)
+print("Release Year:", Release_year)
+print("Genre:", Genre)
+print("Duration (minutes):", Duration)
+print("Has the movie been awarded?", Awarded)
