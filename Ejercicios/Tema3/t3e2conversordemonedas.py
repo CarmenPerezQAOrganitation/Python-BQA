ad51874c2c7adcb10136da5f2a3da6e27ec08a25
Carmen Pérez <cperezrobles@gmail.com>
2026-06-03 19:46:13 +0200

    Create t3e2conversordemonedas.py

---
+++ Ejercicios/Tema3/t3e2conversordemonedas.py
@@ -0,0 +1,12 @@
+# Pide a la usuaria la cantidad de euros a convertir
+cantidad_en_euros = float(input("Ingresa la cantidad de euros: "))
+
+#Convierte euros a dólares
+tasa_de_cambio = 1.1  
+cantidad_en_dolares = cantidad_en_euros * tasa_de_cambio
+print(f"{cantidad_en_euros} euros son equivalentes a {cantidad_en_dolares} dólares.")
+
+#Convierte euros a libras
+tasa_de_cambio_libras = 0.87 
+cantidad_en_libras = cantidad_en_euros * tasa_de_cambio_libras
+print(f"{cantidad_en_euros} euros son equivalentes a {cantidad_en_libras} libras.")
