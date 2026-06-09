25dd60a2ad437c3b6631b2340055c2cd56600ca0
Carmen Pérez <cperezrobles@gmail.com>
2026-06-09 18:48:35 +0200

    Update t1e1peliculas.py

--- Ejercicios/Tema1/t1e1peliculas.py
+++ Ejercicios/Tema1/t1e1peliculas.py
@@ -17,12 +17,12 @@ Duración = 120.5
 Premiada = True
 
 # Imprimir la información de la película
-print("Título:", Titulo)
-print("Director:", Director)
-print("Año de lanzamiento:", Año_lanzamiento)
-print("Género:", Genero)
-print("Duración (minutos):", Duración)
-print("¿La película ha sido premiada?", Premiada)
+print("Título: ", Titulo)
+print("Director: ", Director)
+print("Año de lanzamiento: ", Año_lanzamiento)
+print("Género: ", Genero)
+print("Duración (minutos): ", Duración)
+print("¿La película ha sido premiada? ", Premiada)
 
 #Cambiar datos a inglés
 # Cadenas (strings)
