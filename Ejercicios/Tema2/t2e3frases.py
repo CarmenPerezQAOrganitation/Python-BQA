565c42c7ca5cb2b7144ab9fc1622f16a16b75c9e
Carmen Pérez <cperezrobles@gmail.com>
2026-06-09 19:47:44 +0200

    Update t2e3frases.py

--- Ejercicios/Tema2/t2e3frases.py
+++ Ejercicios/Tema2/t2e3frases.py
@@ -4,12 +4,12 @@ frase = input("Ingresa una frase: ")
 #Longitud de la frase
 texto = frase.replace(" ", "")  # Elimina los espacios para contar solo las letras
 longitud = len(texto)
-print(longitud)  
+print("La longitud de la frase es:", longitud)
 
 # Convertir a mayúsculas
 mayusculas = frase.upper()
-print(mayusculas) 
+print("La frase en mayúsculas es:", mayusculas)
 
 # Convertir a minusculas
 minusculas = frase.lower()
-print(minusculas)  
+print("La frase en minusculas es:", minusculas)  
